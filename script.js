9a89fc7f0b8603ecab36fc83cd31e269056e731a
mangosain <k.gosain7140@gmail.com>
2024-01-01 18:40:37 +0530

    added portfolio section

--- script.js
+++ script.js
@@ -172,3 +172,14 @@ function fetchRandomQuote() {
 
 // Fetch a random quote when the page loads
 fetchRandomQuote();
+
+$(function(){
+  $(".filtering").on("click", "span", function () {
+      var a = $(".gallery").isotope({});
+      var e = $(this).attr("data-filter");
+      a.isotope({ filter: e });
+  });
+  $(".filtering").on("click", "span", function () {
+      $(this).addClass("active").siblings().removeClass("active");
+  });
+}) 
