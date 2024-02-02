020934f571208cdba906a688595939a516638af4
mangosain <k.gosain7140@gmail.com>
2024-02-02 13:24:44 +0530

    added bouncing dot for scroll suggestion

--- script.js
+++ script.js
@@ -172,14 +172,3 @@ function fetchRandomQuote() {
 
 // Fetch a random quote when the page loads
 fetchRandomQuote();
-
-$(function(){
-  $(".filtering").on("click", "span", function () {
-      var a = $(".gallery").isotope({});
-      var e = $(this).attr("data-filter");
-      a.isotope({ filter: e });
-  });
-  $(".filtering").on("click", "span", function () {
-      $(this).addClass("active").siblings().removeClass("active");
-  });
-}) 
